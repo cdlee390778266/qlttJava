b6353c589fcf1d6ab78250e0df6c1d07481143b5
wenw <wenw@qianlong2.net>
2017-03-31 16:35:28 +0800

    2017-03-31

---
+++ code/qltt-os/src/main/java/weixin/guanjia/message/service/MessageTemplateService.java
@@ -0,0 +1,14 @@
+package weixin.guanjia.message.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.jeecgframework.core.common.model.json.AjaxJson;
+import org.jeecgframework.core.common.service.CommonService;
+
+import weixin.guanjia.message.entity.MessageTemplate;
+
+
+public interface MessageTemplateService extends CommonService{
+
+}
