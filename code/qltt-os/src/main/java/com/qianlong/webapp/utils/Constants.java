b74cae7e151ab7e6f92b0f7154e1dad7e2d7f3eb
wangkai <kaige9at@hotmail.com>
2017-04-25 12:22:39 +0800

    调整日志输出配置

--- code/qltt-os/src/main/java/com/qianlong/webapp/utils/Constants.java
+++ code/qltt-os/src/main/java/com/qianlong/webapp/utils/Constants.java
@@ -1,5 +1,7 @@
 package com.qianlong.webapp.utils;
 
+import org.jeecgframework.core.util.ResourceUtil;
+
 public class Constants {
 
 	public static final String SCHEME_HTTP = "http";
@@ -9,17 +11,17 @@ public class Constants {
 	/**
 	 * 获取access_token
 	 */
-	public static final String USER_SERV_OBTAINACCESSTOKEN_PATH = "/qltt-us/token/getAccessToken";
+	public static final String USER_SERV_OBTAINACCESSTOKEN_PATH = ResourceUtil.getConfigByName("user.serv.context") + "token/getAccessToken";
 	
 	/**
 	 * 账号登录(第三方验证授权登录)
 	 */
-	public static final String USER_SERV_TDPART_AUTH_LOGIN = "/qltt-us/acctlogin/login002";
+	public static final String USER_SERV_TDPART_AUTH_LOGIN = ResourceUtil.getConfigByName("user.serv.context") + "acctlogin/login002";
 	
 	/**
 	 * 账号开通(手机号识别)
 	 */
-	public static final String USER_SERV_ACCT_OPEN = "/qltt-us/acctopen/open001";
+	public static final String USER_SERV_ACCT_OPEN = ResourceUtil.getConfigByName("user.serv.context") + "acctopen/open001";
 	
 	public static final String LOGIN_USER_ACCOUNT = "login.user.account";
 	
