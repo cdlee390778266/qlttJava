cf6d3ba2d1809e8fd29377027ebf201df648f08e
wangkai <kaige9at@hotmail.com>
2017-05-16 16:16:22 +0800

    完成推送设置的开发

--- code/qltt-os/src/main/java/com/qianlong/webapp/controller/SettingController.java
+++ code/qltt-os/src/main/java/com/qianlong/webapp/controller/SettingController.java
@@ -11,9 +11,12 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.qianlong.webapp.domain.AuthResultEntity;
+import com.qianlong.webapp.domain.JSONEntity;
 import com.qianlong.webapp.domain.PushFreqEntity;
 import com.qianlong.webapp.domain.PushNumEntity;
 import com.qianlong.webapp.domain.PushScopeEntity;
@@ -67,9 +70,28 @@ public class SettingController {
 		PushScopeEntity pushScope = settingService.getPushScope(user.getTtacct());
 		PushNumEntity pushNum = settingService.getPushNum(user.getTtacct());
 		
-		model.put("pushFreq", pushFreq);
-		model.put("pushScope", pushScope);
-		model.put("pushNum", pushNum);
+		model.put("pushFreq", pushFreq.getPushFreq());
+		model.put("pushScope", pushScope.getPushScope());
+		model.put("scopePrm", pushScope.getScopePrm());
+		model.put("pushNum", pushNum.getPushNum());
 		return new ModelAndView("qianlong/setting", model);
+	}
+	
+	@RequestMapping("freq")
+	@ResponseBody
+	public JSONEntity setPushFreq(HttpServletRequest request, @RequestParam(value = "pushFreq", required = true)Integer pushFreq) {
+		AuthResultEntity user = (AuthResultEntity)request.getSession().getAttribute(Constants.LOGIN_USER_ACCOUNT);
+		settingService.setPushFreq(user.getTtacct(), pushFreq);
+		return new JSONEntity(0, null, null);
+	}
+	
+	@RequestMapping("scope")
+	@ResponseBody
+	public JSONEntity setPushScope(HttpServletRequest request, 
+			@RequestParam(value = "pushScope", required = true)Integer pushScope,
+			@RequestParam(value = "scopePrm", required = true)Integer scopePrm) {
+		AuthResultEntity user = (AuthResultEntity)request.getSession().getAttribute(Constants.LOGIN_USER_ACCOUNT);
+		settingService.setPushScope(user.getTtacct(), pushScope, scopePrm);
+		return new JSONEntity(0, null, null);
 	}
 }
