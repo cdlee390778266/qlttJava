e0ce2a7881e66fa23474917370fa0fa04e4e41ed
wenw <wenw@qianlong2.net>
2017-05-24 14:47:31 +0800

    java

--- code/qltt-os/src/main/java/weixin/guanjia/message/task/MyBusProcess.java
+++ code/qltt-os/src/main/java/weixin/guanjia/message/task/MyBusProcess.java
@@ -43,7 +43,7 @@ public class MyBusProcess implements IBusProcess {
 					String remark = eventpush.getEvdetail()+"\n"+epush.getSummary()+"(查看更多请点击详情,内容当日有效)";//remark
 					//TODO:详情url
 					String url = ResourceUtil.getConfigByName("domain")
-							+"webapp/getMessage/query1001.do?eventdate="+data1.format(data.parse(content.getEp().getOcctime()))
+							+"/webapp/getMessage/query1001.do?eventdate="+data1.format(data.parse(content.getEp().getOcctime()))
 							+"&eventno="+content.getEp().getEventno()
 							+"&ttacct="+epush.getTtacct()+"&svcchnl="+epush.getSvcchnlValue();
 					tep.setUrl(url);
@@ -82,7 +82,7 @@ public class MyBusProcess implements IBusProcess {
 					
 					//TODO:详情url
 					String url =ResourceUtil.getConfigByName("domain")
-							+"webapp/getMessage/query1002.do?eventdate="+data1.format(data.parse(_eventpush.getOcctime()))
+							+"/webapp/getMessage/query1002.do?eventdate="+data1.format(data.parse(_eventpush.getOcctime()))
 							+"&eventno="+_eventpush.getEventno()
 							+"&ttacct="+_eventpush.getTtacct()+"&svcchnl="+_eventpush.getSvcchnlValue();
 					tep.setUrl(url);
